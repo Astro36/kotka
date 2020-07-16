d60c88c709286a673a6a25ad73e3320755bede17
Seungjae Park <astro.psj@gmail.com>
2020-07-16 17:58:48 +0900

    Add project base

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# kotka
-Korean Obfuscation ToolKit Advanced
+# Kotka
+
+> Korean Obfuscation ToolKit Advanced
+
+[![PyPI](https://img.shields.io/pypi/v/kotka?logo=pypi&logoColor=white&style=for-the-badge)](https://pypi.org/project/kotka/)
+[![Python](https://img.shields.io/pypi/pyversions/kotka?logo=python&logoColor=white&style=for-the-badge)](https://www.python.org/)
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/Astro36/kotka/Python?logo=github&logoColor=white&style=for-the-badge)](https://github.com/Astro36/kotka/actions)
+[![Downloads](https://img.shields.io/pypi/dm/kotka?logo=pypi&logoColor=white&style=for-the-badge)](https://pypi.org/project/kotka/)
+[![License](https://img.shields.io/pypi/l/kotka?style=for-the-badge)](./LICENSE)
---
+++ setup.py
@@ -0,0 +1,35 @@
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+basedir = path.abspath(path.dirname(__file__))
+with open(path.join(basedir, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='kotka',
+    version='0.1.0',
+    description='Korean Obfuscation ToolKit Advanced',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Seungjae Park',
+    author_email='astro.psj@gmail.com',
+    url='https://github.com/Astro36/kotka',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Korean',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    license='MIT',
+    keywords='korean-nlp, korean-text-processing, hangul, toolkit',
+    python_requires='>=3',
+)
