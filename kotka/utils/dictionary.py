5efb36273d2d81ec3e0cc5c879f6ce7877139c73
Seungjae Park <astro.psj@gmail.com>
2020-07-28 18:20:34 +0900

    Change OrderedDict type for supporting Python 3.6

--- kotka/utils/dictionary.py
+++ kotka/utils/dictionary.py
@@ -1,6 +1,7 @@
+from collections import OrderedDict
 from functools import cmp_to_key
-from typing import Callable, Dict, OrderedDict, Tuple
+from typing import Callable, Dict, Tuple
 
 
-def sort_by_key(dictionary: Dict[str, str], cmp: Callable[[Tuple[str, str], Tuple[str, str]], int]) -> OrderedDict[str, str]:
+def sort_by_key(dictionary: Dict[str, str], cmp: Callable[[Tuple[str, str], Tuple[str, str]], int]) -> Dict[str, str]:
     return OrderedDict(sorted(dictionary.items(), key=cmp_to_key(cmp)))
